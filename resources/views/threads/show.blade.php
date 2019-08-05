f5e1b821e27201f98373bf9060c94e3872e93b05
JoshFried <josh.fried13@gmail.com>
2019-08-05 13:20:47 -0400

    fixed formatting on show.blade.php

--- resources/views/threads/show.blade.php
+++ resources/views/threads/show.blade.php
@@ -17,16 +17,23 @@
 
 <div class="container">
         <div class="row justify-content-center">
-            <div class="col-md-8">
-                <div class="card">
-                    <div class="card-header">{{ $thread->title }}</div>
+            <div class="col-md-8  ">
                     @foreach ($thread->replies as $reply)
-                    <div class="card-body">
-                        {{ $reply->body }}
-                        <hr>
+                    <div class="card mt-4">
+
+                        <div class="card-header" >
+                            <a href="">
+                                {{ $reply->owner->name }}
+                            </a> 
+                            said
+                            {{ $reply->created_at->diffForHumans() }} ...
+                        </div>
+                        
+                        <div class="card-body">
+                            {{ $reply->body }}
+                        </div>
                     </div>
-                    @endforeach
-                </div>
+                    @endforeach 
             </div>
         </div>
     </div>
