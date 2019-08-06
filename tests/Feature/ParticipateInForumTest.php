a51523be98d86b4bc02927817b5b452da769fa25
JoshFried <josh.fried13@gmail.com>
2019-08-06 13:24:05 -0400

    test for validation errors

--- tests/Feature/ParticipateInForumTest.php
+++ tests/Feature/ParticipateInForumTest.php
@@ -9,36 +9,34 @@ use Illuminate\Foundation\Testing\DatabaseMigrations;
 
 class ParticipateInForumTest extends TestCase
 {
-
     use DatabaseMigrations;
-
-
-    /** @test */
-
-    // public function unauthenticated_users_may_not_add_replies() { 
-
-
-    //     $thread = factory('App\Thread')->create();
-
-    //     $reply = factory('App\Reply')->create();
-    //     $this->post($thread->path().'/replies/', $reply->toArray());
-    // }
 
     /** @test */
     public function an_authenticated_user_may_participate_in_forum_threads()
     {
-        //given we have an authenticated user 
+        //given we have an authenticated user
         $this->be($user = factory('App\User')->create());
 
         //and an existing thread
-        $thread = create('App\Thread'); 
+        $thread = create('App\Thread');
 
         //when the user adds a rpely to the thread
         $reply = make('App\Reply');
-        $this->post($thread->path().'/replies', $reply->toArray()); 
+        $this->post($thread->path().'/replies', $reply->toArray());
 
         //then their reply should be visible on the page
         $this->get($thread->path())
             ->assertSee($reply->body);
+    }
+
+    /** @test */
+    public function a_reply_requires_a_body()
+    {
+        $this->signIn();
+        $thread = create('App\Thread');
+        $reply = make('App\Reply', ['body' => null]);
+
+        $this->post($thread->path() . '/replies', $reply->toArray())
+            ->assertSessionHasErrors('body');
     }
 }
