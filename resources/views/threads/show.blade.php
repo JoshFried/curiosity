d7fb03bb239c9a285eab35e8d607e73c669b9ae0
JoshFried <josh.fried13@gmail.com>
2019-08-05 20:18:09 -0400

    added reply form

--- resources/views/threads/show.blade.php
+++ resources/views/threads/show.blade.php
@@ -5,11 +5,11 @@
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-                
-                
+
+
                 <div class="card-header">
                     <a href="">{{ $thread->creator->name }}</a> posted
-                        {{ $thread->title }} 
+                    {{ $thread->title }}
                 </div>
 
                 <div class="card-body">
@@ -20,13 +20,33 @@
     </div>
 </div>
 
-<div class="container">
+<div class="container" style="padding-bottom: 30px">
+    <div class="row justify-content-center">
+        <div class="col-md-8  ">
+            @foreach ($thread->replies as $reply)
+            @include('threads.reply')
+            @endforeach
+        </div>
+    </div>
+</div>
+
+@if (auth()->check())
+    <div class="container">
         <div class="row justify-content-center">
             <div class="col-md-8  ">
-                    @foreach ($thread->replies as $reply)
-                        @include('threads.reply')
-                    @endforeach 
+
+                <form action="{{ $thread->path() . '/replies'}}" method="post">
+                    @csrf
+                    <div class="form-group">
+                        <textarea id="body" class="form-control" type="text" name="body" placeholder="Have somethingto say?" rows="5" ></textarea>
+                        <button class="btn btn-default" type="submit">Post</button>
+                    </div>
+                </form>
+            
             </div>
         </div>
     </div>
+@else 
+    <p class="text-center">Please <a href="{{ route('login') }}">sign in</a> to post!</p>
+@endif
 @endsection
